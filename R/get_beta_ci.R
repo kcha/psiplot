debe67115c16a820a45052468b626eeda6a27f3a
Kevin Ha <k.ha@mail.utoronto.ca>
2014-12-03 23:03:27 -0500

    Add missing parseQual function from diff

--- R/get_beta_ci.R
+++ R/get_beta_ci.R
@@ -6,6 +6,22 @@
 #' @param q original PSI plus quality scores WITHOUT the first 7 columns
 #' @return Confidence intevals of PSI values
 get_beta_ci <- function(q) {
+  #  This function takes a qual and returns c(post_alpha, post_beta)
+  #  Increments by prior alpha and prior distribution beta, uniform by default
+  parseQual <- function(qual, prior_alpha=1, prior_beta=1) {
+    if(is.na(qual) || !grepl("@", qual)) { return(c(prior_alpha, prior_beta)) }  ## for INT NA Columns
+    res <- unlist(strsplit(unlist(strsplit(as.character(qual), "@"))[2], ","))
+    if(is.na(res[1]) || is.na(res[2])) { return(c(prior_alpha, prior_beta)) }
+    if(is.null(res[1]) || is.null(res[2])) { return(c(prior_alpha, prior_beta)) }
+    if(res[1] == "NA" || res[2] == "NA") { return(c(prior_alpha, prior_beta)) }
+    res <- as.numeric(res)
+    if(is.nan(res[1]) || is.nan(res[2])) { return(c(prior_alpha, prior_beta)) }
+    if(is.infinite(res[1]) || is.infinite(res[2])) { return(c(prior_alpha, prior_beta)) }
+    res[1] <- res[1] + prior_alpha
+    res[2] <- res[2] + prior_beta
+    res
+  }
+
   betaCI <- function(betaDist, percentile = c(0.05, 0.95)) {
     quantile(betaDist, p=percentile, na.rm = T)
   }
