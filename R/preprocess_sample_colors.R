b6aaf8fb3fa27668e747654837fe0d6f2252fb30
kha <k.ha@mail.utoronto.ca>
2014-12-06 00:35:45 -0500

    Add support for accepting config as data frame
     - Update documentation and variable name

--- R/preprocess_sample_colors.R
+++ R/preprocess_sample_colors.R
@@ -5,7 +5,7 @@
 #' corresponding colors are taken from a pre-defined tab-delimited file
 #' (see \code{details}).
 #'
-#'@usage preprocess_sample_colors(psi, database)
+#'@usage preprocess_sample_colors(psi, config)
 #'
 #' @details
 #' \code{preprocess_sample_colors} depends on a pre-defined "sample inventory"
@@ -30,70 +30,78 @@
 #' }
 #'
 #' The \emph{SampleName} must match the column names in \code{psi}. It is possible
-#' for \code{database} to contain more samples than the \code{psi}. In this case,
+#' for \code{config} to contain more samples than the \code{psi}. In this case,
 #' the extra samples will be ignored. It is also possible that
-#' \code{database} contains only a subset of the samples in \code{psi}. In this
-#' case, only the samples specified in the \code{database} will be plotted and everything
+#' \code{config} contains only a subset of the samples in \code{psi}. In this
+#' case, only the samples specified in the \code{config} will be plotted and everything
 #' else is ignored.
 #'
-#' @param data a \emph{n} x \emph{m} data frame of PSI values where \emph{n} is the number of AS events
+#' @param data a \emph{n} x \emph{2*m} data frame of PSI and quality score values where \emph{n} is the number of AS events
 #' and \emph{m} is the number of samples.
-#' @param database filename of the samples database in tab-delimited format.
+#' @param config filename of the configuration file for \code{data}. Also
+#' accepts \emph{m'} x \emph{4} data frame of the configuration file, \code{m <=
+#' m}
+#' @export
 #' @return
 #' A list containing:
 #'  \item{data}{data frame of PSI values with columns re-ordered}
+#'  \item{qual}{data frame of quality scores with columns re-ordered}
 #'  \item{col}{vector of colors corresponding to the re-ordered columns}
 #'  \item{group.index}{list of column indices corresponding to each \emph{GroupName}}
 #'  \item{group.col}{vector of colors corresponding to each \emph{GroupName}}
-preprocess_sample_colors <- function(data, database) {
+#' @examples
+#' reorderedpsi <- preprocess_sample_colors(mm.psi, config = mm.psi.config)
+preprocess_sample_colors <- function(data, config) {
    R <- list()
 
-   if (is.null(database)) {
+   if (is.null(config)) {
      mycols <- rep("black", ncol(data) / 2)
      R <- list(data=data[, seq(1, ncol(data), 2)],
                qual=data[, seq(2, ncol(data), 2)],
                col=mycols, group.index=NULL, group.col=NULL)
    } else {
-     db <- read.table(database, header = T, sep="\t", comment.char="")
-
-     # check input file
-     if (ncol(db) < 4) {
-       stop("The tissues database file is not formatted correctly. Please
-          double check")
+     if (!is.character(config)) {
+       config <- read.table(config, header = T, sep="\t", comment.char="", 
+                            stringsAsFactors=FALSE)
      }
 
-     # check if all samples in input data is in the database
-     #unk.samples <- colnames(data) %in% db$SampleName
+     # check input file
+     if (all(colnames(config) == c("Order", "SampleName", "GroupName", "RColorCode"))) {
+       stop("Incorrect formatting of headers in config")
+     }
+
+     # check if all samples in input data is in the config
+     #unk.samples <- colnames(data) %in% config$SampleName
      #if (!all(unk.samples)) {
      #s <- colnames(data)[!unk.samples]
-     #stop(paste("The following samples are not in the tissues database:",
+     #stop(paste("The following samples are not in the tissues config:",
      #paste(s, collapse=", ")))
      #}
 
      # keep only tissue groups that are present in input data
      # (to take into account samples that might have been excluded)
-     db <- db[db$SampleName %in% colnames(data),]
+     config <- config[config$SampleName %in% colnames(data),]
 
      # Re-order the PSI table
-     db <- db[order(db$Order),]
-     db$Order <- 1:nrow(db)
-     new.column.idx <- sapply(db$SampleName,
+     config <- config[order(config$Order),]
+     config$Order <- 1:nrow(config)
+     new.column.idx <- sapply(config$SampleName,
                               function(x) which(colnames(data) == x))
 
      data.new <- data[,new.column.idx]
 
      # Generate a corresponding color code sequence
-     mycols <- db$RColorCode
-     names(mycols) <- db$SampleName
+     mycols <- config$RColorCode
+     names(mycols) <- config$SampleName
 
      # Store indices of columns for each group
-     groups <- unique(db$GroupName)
+     groups <- unique(config$GroupName)
      mygroups <- list()
      mygroupcol <- rep(NA, length(groups))
      for (i in 1:length(groups)) {
        mygroups[[i]] <- which(colnames(data.new) %in%
-                                db[db$GroupName == groups[i],"SampleName"])
-       mygroupcol[i] <- as.character(db[db$GroupName == groups[i],
+                                config[config$GroupName == groups[i],"SampleName"])
+       mygroupcol[i] <- as.character(config[config$GroupName == groups[i],
                                         "RColorCode"][1])
      }
      names(mygroups) <- groups
