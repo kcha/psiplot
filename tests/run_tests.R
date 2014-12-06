5f5cee9cf49ecb21cfe7838d6d9ba2081cd2c59b
Kevin Ha <k.ha@mail.utoronto.ca>
2014-12-06 13:09:59 -0500

    Initial import of unit tests

---
+++ tests/run_tests.R
@@ -0,0 +1,5 @@
+#!/usr/bin/env Rscript
+library(testthat)
+library(psiplot)
+
+test_check("psiplot")
---
+++ tests/testthat/test_preprocess_sample_colors.R
@@ -0,0 +1,12 @@
+context("Test re-ordering of event data using configuration file")
+
+config <- data.frame(Order=c(1,2),
+                     SampleName=c("Sample1", "Sample4"),
+                     GroupName=c("Muscle", "Neural"),
+                     RColorCode=c("red", "blue")
+                     )
+
+test_that("Only samples in config are retained", {
+    r <- preprocess_sample_colors(mm.psi, config = config)
+    expect_equal(ncol(r$data), nrow(config))
+    })
